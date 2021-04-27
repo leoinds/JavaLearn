a366971ec87bd16007ab56a0b698ce1729fc0d60
Leonid Ermakovich <lermakovich@inbox.ru>
2021-04-28 00:12:13 +0300

    test first commit

---
+++ src/main/java/LearnMain.java
@@ -0,0 +1,45 @@
+public class LearnMain {
+    public static void main(String[] args) {
+        int count;
+        count = 1;
+        System.out.println(count == 1);
+        System.out.println(count != 1);
+        System.out.println(count < 1);
+        System.out.println(count > 0);
+        System.out.println(count <= 1);
+        System.out.println(count >= 2);
+        Object object1 = new object();
+        Object object2 = object1;
+        Object object3 = null;
+        System.out.println(object1 == object2);
+        System.out.println(object3 == null);
+        Float r;
+        r = 1 / 3f * 3;
+        System.out.println(1.f == r);
+
+        Integer i2, i3;
+        i2 = -128;
+        i3 = -128;
+        System.out.println(i2 == i3);
+        int x;
+        int y;
+        x = 1;
+        y = 2;
+        boolean a = x == y;
+        boolean b = x != y;
+        System.out.println();
+        System.out.println(x++ != y++ || x++ == y++);
+        System.out.println("x=" + x + ", y=" + y);
+
+        System.out.println();
+        if (x < y)
+        {
+            System.out.println("x меньше y");
+            System.out.println("y больше x");
+        }
+    else{
+            System.out.println("x не меньше y");
+            System.out.println("у не больше х");
+        }
+    }
+}
---
+++ src/test/java/var/var.java
@@ -0,0 +1,5 @@
+package var;
+
+public class var<n> {
+    String n = "abc";
+}

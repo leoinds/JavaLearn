ae96d0c4d8882cf0ea679b78414d3922150a4226
Leonid Ermakovich <lermakovich@inbox.ru>
2021-04-28 00:53:50 +0300

    add a leo int

--- src/main/java/LearnMain.java
+++ src/main/java/LearnMain.java
@@ -43,7 +43,7 @@ public class LearnMain {
             System.out.println("x не меньше y");
             System.out.println("у не больше х");
             System.out.println();
-            int leo=123;
+            int leo=12;
         }
     }
 }
